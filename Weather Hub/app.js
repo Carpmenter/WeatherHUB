62d11221d855cac22fa26aa37bf287afb9687ef5
Nick Carpenter <nick.carpenter@quadrotech-it.com>
2019-02-15 17:41:02 -0600

    Initial Commit to add project files

---
+++ Weather Hub/app.js
@@ -0,0 +1,7 @@
+/*** Main JavaScript logic for Weather Hub */
+// API Key: f20d0afcce1a8e9378946a0b3d0f107e
+
+// Search button
+document.getElementById('search-btn').addEventListener('click', function(){
+
+});
