1152ed8ec14be88053e931aeb91217a270819c34
Nick Carpenter <nickcarpenter1017@gmail.com>
2019-02-15 21:09:05 -0600

    Updated header formatting and implemented basic data retrieval

--- Weather Hub/app.js
+++ Weather Hub/app.js
@@ -1,7 +1,16 @@
 /*** Main JavaScript logic for Weather Hub */
 // API Key: f20d0afcce1a8e9378946a0b3d0f107e
+$(document).ready(function() {
+    
+    // Search button
+var wData;
 
-// Search button
-document.getElementById('search-btn').addEventListener('click', function(){
+    $.getJSON('https://api.openweathermap.org/data/2.5/weather?q=Fargo&APPID=f20d0afcce1a8e9378946a0b3d0f107e&units=imperial', function(data) {
+        wData = data;
+        //wData = data;
+    });
+    document.getElementById('test').addEventListener('click', function(){
+        document.getElementById('cur-weather').textContent ='Location: ' + wData.name + '\nTemperature: ' + wData.main.temp + ' Degrees F' + '\nWind Speed: ' + wData.wind.speed;
+    });
 
 });
